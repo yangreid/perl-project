2ad41e2c9ec7f4e17c95bc62e0d730b71bf8497a
Yang Reid <yangtao584@gmail.com>
2012-09-22 21:41:51 +0800

    add the two perl process fork's example

---
+++ perl_fork/fork_test_2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#------------------------------------
+# fork-test.pl
+print "Program started, pid=$$.\n";
+if ($child_pid = fork()) {
+	print "I'm parent, my pid=$$, child's pid=$child_pid.\n";
+	waitpid($child_pid,0);
+} else {
+	print "I'm child, pid=$$.\n";
+	sleep 10;
+	exec("date");	
+}
